d19a35962a596bd694858a2895d543a73a9713ed
AraoSibindeJr <araosibinde098junior@gmail.com>
2025-02-13 13:41:02 +0200

    feat: Item list added

---
+++ src/App2.jsx
@@ -0,0 +1,9 @@
+// Arrow function [rafce] atalho para arrow function
+
+import React from "react";
+
+const App2 = () => {
+  return <div>App2</div>;
+};
+
+export default App2;
---
+++ src/components/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ItemList from "./itemList";
+
+const Main = () => {
+  return (
+    <div className="main">
+      <ItemList />
+      <ItemList />
+    </div>
+  );
+};
+
+export default Main;
